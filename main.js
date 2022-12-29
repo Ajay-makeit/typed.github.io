78f326f1b0cc907b9727d3349856351b7d890f42
Ajay-makeit <94294721+Ajay-makeit@users.noreply.github.com>
2022-12-30 01:19:02 +0530

    uploaded files
    uploaded the required files

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed2 = new Typed('.autotype', {
+    strings: ["I ' m", ' Botchu Ajay', 'Web Desingner', 'Tech Enthusiast', 'Public Speaker',  'VcodewithU &times; &copy;'],
+    typeSpeed: 40,
+    backSpeed: -1,
+    fadeOut: true,
+    loop: true
+  });
